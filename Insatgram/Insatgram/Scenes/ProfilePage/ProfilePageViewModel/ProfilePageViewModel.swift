d4c8b540816d528ab4fddd4063e412d8e43b9be7
andria.gv <gvaramiaandria1@gmail.com>
2024-11-23 00:24:23 +0400

    add user mock for testing

--- Insatgram/Insatgram/Scenes/ProfilePage/ProfilePageViewModel/ProfilePageViewModel.swift
+++ Insatgram/Insatgram/Scenes/ProfilePage/ProfilePageViewModel/ProfilePageViewModel.swift
@@ -5,6 +5,34 @@
 //  Created by irakli kharshiladze on 22.11.24.
 //
 
+import UIKit
+
 final class ProfilePageViewModel {
     
+    struct Movie {
+        let id: Int?
+        let image: UIImage?
+    }
+    
+    let droebiti: [Movie] = [
+        Movie(id: 10, image: UIImage(named: "Profile")),
+        Movie(id: 11, image: UIImage(named: "Profile")),
+        Movie(id: 12, image: UIImage(named: "Profile")),
+        Movie(id: 13, image: UIImage(named: "Profile")),
+        Movie(id: 14, image: UIImage(named: "Profile")),
+        Movie(id: 15, image: UIImage(named: "Profile")),
+        Movie(id: 16, image: UIImage(named: "Profile")),
+        Movie(id: 17, image: UIImage(named: "Profile")),
+        Movie(id: 18, image: UIImage(named: "Profile")),
+        Movie(id: 19, image: UIImage(named: "Profile")),
+        Movie(id: 20, image: UIImage(named: "Profile")),
+        Movie(id: 21, image: UIImage(named: "Profile")),
+        Movie(id: 22, image: UIImage(named: "Profile")),
+        Movie(id: 23, image: UIImage(named: "Profile")),
+        Movie(id: 24, image: UIImage(named: "Profile")),
+        Movie(id: 25, image: UIImage(named: "Profile")),
+        Movie(id: 26, image: UIImage(named: "Profile")),
+        Movie(id: 27, image: UIImage(named: "Profile")),
+        Movie(id: 28, image: UIImage(named: "Profile"))
+    ]
 }
