4c0cff797339938df1e501c6911c50d1b0b12b8d
andria.gv <gvaramiaandria1@gmail.com>
2024-11-23 20:38:49 +0400

    add user model

--- Insatgram/Insatgram/Models/UserProfileMedia.swift
+++ Insatgram/Insatgram/Models/UserProfileMedia.swift
@@ -1,37 +1,33 @@
 //
-//  UserMedia.swift
+//  UserProfileMedia.swift
 //  Insatgram
 //
 //  Created by Apple on 23.11.24.
 //
 
-
 import Foundation
 
-struct UserProfileMedia: Codable {
+struct UserMediaResponse: Decodable {
+    let data: [UserMedia]
+}
+
+struct UserMedia: Decodable {
     let id: String
-    let lowResolutionImage: Image
-
-    enum CodingKeys: String, CodingKey {
-        case id
-        case images
-    }
-
-    enum ImagesKeys: String, CodingKey {
-        case lowResolution = "low_resolution"
-    }
-
-    struct Image: Codable {
-        let url: String
-        let width: Int
-        let height: Int
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(String.self, forKey: .id)
+    let images: Images
+    
+    struct Images: Decodable {
+        let lowResolution: ImageResolution
         
-        let imagesContainer = try container.nestedContainer(keyedBy: ImagesKeys.self, forKey: .images)
-        lowResolutionImage = try imagesContainer.decode(Image.self, forKey: .lowResolution)
+        enum CodingKeys: String, CodingKey {
+            case lowResolution = "low_resolution"
+        }
+        
+        struct ImageResolution: Decodable {
+            let url: String
+            let width: Int
+            let height: Int
+        }
     }
 }
+
+
