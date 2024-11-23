145bae39afcbc1cf17f2294c949f99fbf82b4324
andria.gv <gvaramiaandria1@gmail.com>
2024-11-23 20:43:24 +0400

    add Image Extension

--- Insatgram/Insatgram/Support/Extension/UIImageViewExtension.swift
+++ Insatgram/Insatgram/Support/Extension/UIImageViewExtension.swift
@@ -5,4 +5,33 @@
 //  Created by Apple on 23.11.24.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView{
+
+    func imageFrom(url:URL){
+
+        DispatchQueue.global().async { [weak self] in
+
+            if let data = try? Data(contentsOf: url){
+
+                if let image = UIImage(data:data){
+
+                    DispatchQueue.main.async{
+
+                        self?.image = image
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+//newsImageView.imageFrom(url: URL(string: article.urlToImage)!)
+ 
